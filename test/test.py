5362d88fb7a9c50bf6edbbea1a2ba1aee540efab
Yan Wenjun <mylastnameisyan@gmail.com>
2012-11-25 16:50:21 +0900

    add more test and fix some bugs

--- test/test.py
+++ test/test.py
@@ -5,4 +5,13 @@ from JSON_parser import YAJP
 
 
 print YAJP('{"a":[1,2,3], "b":"\\t"}').parse()
+print '*'*20
 print YAJP(open('json/test1.json').read()).parse()
+print '*'*20
+print YAJP(open('json/test2.json').read()).parse()
+print '*'*20
+print YAJP(open('json/test3.json').read()).parse()
+print '*'*20
+print YAJP(open('json/test4.json').read()).parse()
+print '*'*20
+print YAJP(open('json/test5.json').read()).parse()
